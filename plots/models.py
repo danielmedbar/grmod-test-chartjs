989aeb48fc03759cf2a3bdca4e102bcc7bbebe12
danielmedbar <danielmedbar@gmail.com>
2021-02-25 09:41:10 +0000

    add pandas table to front

--- plots/models.py
+++ plots/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
+from datetime import date
+from time import strftime
+import pandas as pd
+
 # Create your models here.
+
+start_date = date(2018,2,1)
+end_date = date(2020,1,1)
+
+cols_date = pd.date_range(start_date, end_date, freq='d', closed=None)
+cols_date = cols_date[cols_date.day==1].strftime('%B %Y')
+
+col1 = ['Funnel']
+cols = col1 + cols_date.to_list()
+
+df = pd.DataFrame(columns=cols)
+df['Funnel'] = ['Traffic','Signup','Active','Conversion1','Payment']
+
+base_model = df.copy()
--- plots/views.py
+++ plots/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
-def create_plot(request):
-  return HttpResponse("Hello, world. This is the site for plots.")
+from .models import base_model
 
+# Create your views here.
 def bar_plots(request):
   labels = ['bcn','mad','val','sev']
   data = [200, 300, 150, 100]
 
+  base_model_html = base_model.to_html()
+
   return render(request, 'plots.html', {
     'labels': labels,
     'data': data,
+    'base_model': base_model_html,
   })
-
